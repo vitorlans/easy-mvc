8561e2ba839f0d1786e1283bb569785ee75b918e
Sebastiao <sebastiaopurcini@gmail.com>
2014-10-10 20:26:17 -0300

    Alteracao do Projeto. ADD layout

--- Easy/App_Start/BundleConfig.cs
+++ Easy/App_Start/BundleConfig.cs
@@ -23,7 +23,11 @@ namespace Easy
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css",
+                                         "~/Content/css/plugins/morris.css",
+                                         "~/Content/css/bootstrap.css",
+                                         "~/Content/css/bootstrap.mim.css",
+                                         "~/Content/css/sb-admin.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
