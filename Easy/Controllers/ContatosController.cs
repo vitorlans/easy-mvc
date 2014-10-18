129cf9768d5ba23e890cac462f2f9783d2004a59
vttor <vitor_hs@live.com>
2014-10-18 19:27:33 -0300

    Contatos/Detalhes/Add

--- Easy/Controllers/ContatosController.cs
+++ Easy/Controllers/ContatosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Easy.Models;
 
 namespace Easy.Controllers
 {
@@ -11,9 +12,10 @@ namespace Easy.Controllers
         //
         // GET: /Contatos/
 
-        public ActionResult Index()
+        public ActionResult Index(Usuario user)
         {
-            return View();
+            var lista = user.ContatosLTotal();
+            return View(lista);
         }
 
         public ActionResult Add() {
@@ -22,10 +24,51 @@ namespace Easy.Controllers
         
         }
 
-        public ActionResult Detalhes(string id)
+        [HttpPost]
+        public ActionResult Add(Usuario user, string CheckUserSist, string CheckEnvConv)
         {
+
+            if (CheckUserSist == null)
+            {
+
+                user.UsuarioSistema = false;
+            }
+            else
+            {
+
+                user.UsuarioSistema = true;
+            }
+
+            if (CheckEnvConv == null)
+            {
+                user.LiberaConvite = false;
+            }
+            else
+            {
+
+                user.LiberaConvite = true;
+            }
+
             
             return View();
+
+        }
+        public ActionResult Detalhes(string id)
+        {
+
+            Usuario user = new Usuario();
+            user.UsuarioSistema = true;
+            user.Nome = "Vitor Santos";
+            user.Sobrenome = "Silva";
+            user.Telefone = "19 3582-5664";
+            user.Email = "vitor_hs@live.com";
+            user.Endereco = "Mauricio Affonso Moreno";
+            user.Cidade = "Santa Rita do Passa Quatro";
+            user.Bairro="Vila Norte";
+            user.Cep="13670000";
+            user.LiberaConvite=true;
+
+            return View(user);
 
         }
     }
--- Easy/Models/Usuario.cs
+++ Easy/Models/Usuario.cs
@@ -10,6 +10,7 @@ namespace Easy.Models
         public int IdUser { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
+        public string Email { get; set; }
         public string Senha { get; set; }
         public string Senha1 { get; set; }
         public string Endereco { get; set; }
@@ -17,9 +18,50 @@ namespace Easy.Models
         public string Cidade { get; set; }
         public string Cep { get; set; }
         public string Telefone { get; set; }
-        public string UsuarioSistema { get; set; }
-        public string LiberaConvite { get; set; }
-        public string Status { get; set; }
+        public bool UsuarioSistema { get; set; }
+        public bool LiberaConvite { get; set; }
+        public bool Status { get; set; }
+
+
+        public List<Usuario> ContatosLTotal()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                var x = 0;
+               while(x < 3)
+                {
+                    lista.Add
+                        (
+                        new Usuario
+                        {
+                            UsuarioSistema = true,
+                            Nome = "Vitor Santos",
+                            Sobrenome = "Silva",
+                            Telefone = "19 3582-5664",
+                            Email = "vitor_hs@live.com",
+                            Endereco = "Mauricio Affonso Moreno",
+                            Cidade = "Santa Rita do Passa Quatro",
+                            Bairro = "Vila Norte",
+                            Cep = "13670000",
+                            LiberaConvite = true
+                        }
+                        );
+                    x++;
+                }
+               lista[1].IdUser = 1;
+               lista[1].Nome = "Sebastião Purcini";
+
+               lista[2].IdUser = 2;
+               lista[2].Nome = "Nathan Bernardes";
+
+
+            }
+            catch { }
+
+            return lista;
+        }
 
     }
 }
