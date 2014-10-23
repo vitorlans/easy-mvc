c47ac620b15e745f18fd806f7acf554a4d7bf205
nathanbernardes22 <nathanbernardes7@gmail.com>
2014-10-22 22:26:17 -0200

    Criação das classes DAOCompromissos, Empresas e Notas

--- Easy/Models/Compromissos.cs
+++ Easy/Models/Compromissos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,7 +13,9 @@ namespace Easy.Models
         public string Descricao {get;set;}
         public DateTime DataInicio {get;set;}
         public DateTime DataTermino {get;set;}
+        //Status será inserido como (T)erminado, (C)ancelado ou em (A)ndamento
         public Boolean Status {get;set;}
-        public Usuario Usuario{get;set;}
+        public Usuario Usuario {get;set;}
+        public Empresas Empresa { get; set; }     
     }
 }
