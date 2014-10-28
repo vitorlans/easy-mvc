e5a80adfa4d835c49e6553559628c091701881f0
Sebastiao <sebastiaopurcini@gmail.com>
2014-10-28 19:07:03 -0200

    IASAS

--- Easy/Controllers/TarefasController.cs
+++ Easy/Controllers/TarefasController.cs
@@ -56,14 +56,36 @@ namespace Easy.Controllers
         [HttpPost]
         public ActionResult AtualizaTarefa(Tarefas tar)
         {
+            string prior = "";
+
+            if (tar.Prioridade == "B")
+                prior = "Baixa";
+            else if (tar.Prioridade == "M")
+                prior = "Média";
+            else if (tar.Prioridade == "A")
+                prior = "Alta";
+
             EnvioEmail email = new EnvioEmail();
             DAOTarefas daoTaf = new DAOTarefas();
 
             daoTaf.AtualizaTarefa(tar);
             //email.EnviarTarefaAtualizada(tar);
+            EmailTarefas eTare = new EmailTarefas();
+
+            eTare.Para = "";
+            eTare.Assunto = "Alteração de Tarefa";
+            eTare.Titulo = "Alteração Realizada em Tarefa";
+            eTare.Descricao = "Algumas alterações foram realizadas em uma Tarefa destinada a você.\nVerifique abaixo as informações:";
+            eTare.Quando = Convert.ToString(DateTime.Now);
+            eTare.Mensagem = "Descrição: "+tar.Descricao+"\nData de Início: "+tar.DtInicio+"\nData de Término: "+tar.DtFim+"\nPrioridade: "+prior;
+            eTare.UrlAceita = "";
+            eTare.UrlRejeita = "";
+
+            eTare.Send();
 
             Session["AddTarefa"] = 2;
             return RedirectToAction("Index", "Tarefas");
+
         }
 
     }
